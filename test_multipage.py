7e325d7858eeb4766c2564f79ac3eaec459b80dc
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-01-27 14:06:44 +0100

    Update test_multipage.py

--- test_multipage.py
+++ test_multipage.py
@@ -16,9 +16,9 @@ app = MultiPage()
 # st.image(display, width = 400)
 
 st.title("Data Storyteller Application")
-
+logged_in=False
 # Add all your application here
-app.add_page("Login", login.app)
+app.add_page("Login", logged_in = login.app)
 if logged_in == True:
     app.add_page("Change password", chaange_password.app)
 app.add_page("Test2", test2.app)
