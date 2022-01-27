9ab0f095f92b4b950fc7b1f6841d5afb8913788b
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-01-27 12:28:03 +0100

    Update test_multipage.py

--- test_multipage.py
+++ test_multipage.py
@@ -21,10 +21,7 @@ col2.title("Data Storyteller Application")
 
 # Add all your application here
 app.add_page("Upload Data", data_upload.app)
-app.add_page("Change Metadata", metadata.app)
-app.add_page("Machine Learning", machine_learning.app)
-app.add_page("Data Analysis",data_visualize.app)
-app.add_page("Y-Parameter Optimization",redundant.app)
+
 
 # The main app
 app.run()
