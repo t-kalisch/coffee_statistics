6047241c3f454b322ff181ceaf007c0188570465
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-01-27 13:55:21 +0100

    Update and rename data_upload.py to login.py

--- pages/data_upload.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import numpy as np
-import pandas as pd
-from pages import utils
-
-# @st.cache
-def app():
-    st.markdown("## Data Upload")
---
+++ pages/login.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+from pages import utils
+
+# @st.cache
+def app():
+    st.markdown("## Login")
+    st.write("Please log in with your username and password")
+    
+    user = st.text_input(label="", placeholder="Username")
+    user_pw = st.text_input(label="", type="password", placeholder="Password")
+    login = st.checkbox("Login", help="Log in with your username and password")
