f226a30051046a4c2f15f422891abac69b3aa6e7
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-06-06 17:32:59 -0400

    Update Enter_holidays.py

--- pages/Enter_holidays.py
+++ pages/Enter_holidays.py
@@ -4,26 +4,57 @@ import pandas as pd
 from pages import utils
 
 # @st.cache
-def app():
-    st.markdown("## Login")
-    st.write("Please log in with your username and password")
-    
-    
-    user_data=[['TK', 'akstr!admin2',1],['PB','akstr!admin2',1],['NV',None,None],['DB',None,None],['FLG','baddragon',None],['SHK',None,None],['TB',None,None],['TT',None,None],['RS',None,None]]
-    logged_in=False
-    admin_status=0
+st.subheader("**:calendar:** Enter holidays")
+if admin_status == "1":
+    col1, col2, col3, col4 = st.columns([0.5,1,1,1])
+    month = col1.text_input("Month", placeholder=datetime.date.today().month)
+    year = col2.text_input("Year", placeholder=datetime.date.today().year)
+    person_hol = col3.text_input("Person", placeholder = "User")
+    holidays = col4.text_input("Number of holidays", placeholder=0)
+    if person_hol == "":
+        sub_hol = st.button("Submit holidays", help="Submit holidays for yourself", on_click=submit_holidays, args=(st.session_state.user_name, month, year, holidays))
+        #if sub_hol:
+        #    submit_holidays(st.session_state.user_name, month, year, holidays)
+    else:
+        sub_hol = st.button("Submit holidays", help="Submit holidays for "+person_hol, on_click=submit_holidays, args=(person_hol,month,year,holidays))
+        #if sub_hol:
+        #    submit_holidays(person_hol, month, year, holidays)
 
-    
-    
-    user = st.text_input(label="", placeholder="Username")
-    user_pw = st.text_input(label="", type="password")#, placeholder="Password")
-    login = st.checkbox("Login", help="You will be logged in while the checkbox is ticked")
+    st.write("-" * 34)   
+    st.subheader("All holidays")
+    all_holidays = get_all_holidays(datetime.datetime.now())
+    #print(all_holidays)
+    names=get_members()
+    columns=["ID","Tot. work days"]
+    for i in range(len(names)):
+        columns.append(names[i])
+    #st.write(all_holidays)
+    df=pd.DataFrame(all_holidays,columns=columns)
+    st.dataframe(df, width=1000, height=1000)
 
-    if login:
-        for i in range(len(user_data)):
-            if user == user_data[i][0] and user_pw == user_data[i][1]:
-                admin_status=user_data[i][2]
-                logged_in=True
-    if logged_in==True:
-        st.write("Successfully logged in!")
-    return logged_in
+else:
+    col1, col2, col3 = st.columns([0.5,1,2])
+    month = col1.text_input("Month", placeholder=datetime.date.today().month)
+    year = col2.text_input("Year", placeholder=datetime.date.today().year)
+    holidays = col3.text_input("Number of holidays", placeholder=0)
+    sub_hol = col1.button("Submit", help="Submit holidays")
+    if sub_hol:
+        submit_holidays(st.session_state.user_name, month, year, holidays)
+    st.write("-" * 34)   
+    st.subheader("All holidays")
+    all_holidays = get_all_holidays(datetime.datetime.now())
+    #print(all_holidays)
+    holidays_person=[]
+    names=get_members()
+    columns=["Month","Total work days"]
+    for i in range(len(names)):
+        if names[i] == st.session_state.user_name:
+            columns.append(names[i])
+            for j in range(len(all_holidays)):
+                temp=[]
+                temp.append(all_holidays[j][0])
+                temp.append(all_holidays[j][i+2])
+                temp.append(all_holidays[j][1])
+                holidays_person.append(temp)
+    df=pd.DataFrame(holidays_person,columns=columns)
+    st.dataframe(df, width=1000, height=1000)
