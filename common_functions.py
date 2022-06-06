ab97aa407f6ffe0a81d0db856df45d9b0bcf29c8
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-06-06 17:49:19 -0400

    Update common_functions.py

--- common_functions.py
+++ common_functions.py
@@ -1,7 +1,23 @@
 import streamlit as st
 import mysql.connector as mysql
 
-
+#---------------------------------------- initiates connection to database -----------------------------------------
 #@st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None})
 def init_connection():
     return mysql.connect(**st.secrets["mysql"])
+
+#----------------------------------------- getting all members from database ---------------------------------------
+#@st.cache
+def get_members():
+    db = init_connection()
+    cursor = db.cursor(buffered=True)
+
+    names=[]
+
+    cursor.execute("select name from members")              #getting all members tables
+    mbrs=cursor.fetchall()
+    mbrs=list(mbrs)
+    for i in range(len(mbrs)):
+        names.append(mbrs[i][0])
+    db.close()
+    return names
