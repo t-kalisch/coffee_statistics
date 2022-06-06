4ef797ae4afeae048a023026100e6d45f804abb6
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-06-06 18:09:51 -0400

    Update common_functions.py

--- common_functions.py
+++ common_functions.py
@@ -49,3 +49,26 @@ def get_simple_data():							# getting simple data from database
 
 	db.close()
 	return simple_data
+
+#----------------------------- getting last 10 breaks from database ---------------------------------
+#@st.cache
+def get_last_breaks(last_break):
+	db = init_connection()
+	cursor = db.cursor(buffered=True)
+	cursor.execute("select * from breaks order by id_ext desc limit "+str(last_break))
+	breaks=cursor.fetchall()
+	cursor.execute("select * from drinkers order by id_ext desc limit "+str(last_break))
+	drinkers=cursor.fetchall()
+
+	last_breaks=[]
+	for i in range(len(breaks)):
+		temp=[]
+		date=str(breaks[len(breaks)-i-1][2])+"."+str(breaks[len(breaks)-i-1][3])+"."+str(breaks[len(breaks)-i-1][4])
+		temp.append(breaks[len(breaks)-i-1][1])
+		temp.append(date)
+		temp.append(drinkers[len(drinkers)-i-1][2])
+		temp.append(drinkers[len(drinkers)-i-1][3])
+		last_breaks.append(temp)
+	db.close()
+	return last_breaks
+
