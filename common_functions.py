7b365241c677b4218f4f6e9c99d004c40c78c040
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-06-06 17:46:44 -0400

    Update common_functions.py

--- common_functions.py
+++ common_functions.py
@@ -1,1 +1,6 @@
+import mysql.connector as mysql
 
+
+#@st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None})
+def init_connection():
+    return mysql.connect(**st.secrets["mysql"])
