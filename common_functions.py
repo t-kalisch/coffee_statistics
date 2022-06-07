f8cb70a1334b82ccf81c3ab2a9efd43134bb7ab9
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-06-07 07:46:05 -0400

    Update common_functions.py

--- common_functions.py
+++ common_functions.py
@@ -72,3 +72,58 @@ def get_last_breaks(last_break):
 	db.close()
 	return last_breaks
 
+
+#----------------------------- getting all months from start date to now ---------------------------------
+#@st.cache
+def get_months(first_date):
+    db = init_connection()
+    cursor = db.cursor(buffered=True)
+    
+    month_info=[]
+    months=[]
+    month_id=[]
+
+    cursor.execute("SELECT max(id_ext) FROM breaks")        #getting month names from beginning to current
+    temp=cursor.fetchone()
+    temp=list(temp)
+
+    last_date=datetime.date(int(temp[0][0:4]),int(temp[0][4:6]),int(temp[0][6:8]))
+    for month in months_between(first_date,last_date):
+    #for i in range(months_between(first_date,last_date)):
+        if(month.month<10):
+            month_id.append(str(month.year)+"0"+str(month.month))
+        else:
+            month_id.append(str(month.year)+str(month.month))
+        months.append(month.strftime("%B")[0:3]+" '"+month.strftime("%Y")[2:4])
+    month_info.append(months)
+    month_info.append(month_id)
+    
+    db.close()
+    return month_info
+    
+
+def months_between(start_date, end_date):                   #method to get months between two dates
+    if start_date > end_date:
+        raise ValueError(f"Start date {start_date} is not before end date {end_date}")
+    else:
+        year = start_date.year
+        month = start_date.month
+	
+        #counter=0
+        while (year, month) <= (end_date.year, end_date.month):
+            yield datetime.date(year, month, 1)
+            # Move to the next month.  If we're at the end of the year, wrap around
+            # to the start of the next.
+            #
+            # Example: Nov 2017
+            #       -> Dec 2017 (month += 1)
+            #       -> Jan 2018 (end of year, month = 1, year += 1)
+            #
+            if month == 12:
+                month = 1
+                year += 1
+            else:
+                month += 1
+            #counter += 1
+    #return counter
+
