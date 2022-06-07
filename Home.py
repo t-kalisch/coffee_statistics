2323a3069e84d417951298c861023546754e9c2c
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-06-07 10:27:48 -0400

    Update Home.py

--- Home.py
+++ Home.py
@@ -145,9 +145,9 @@ with st.sidebar:
     left: 0;
     bottom: 0;
     width: 100%;
-    background-color: grey;
+    background-color: white;
     color:  grey;
-    text-align: left;
+    text-align: right;
     }
     </style>
     <div class="footer">
