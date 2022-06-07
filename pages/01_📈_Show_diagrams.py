b07cb8cd55cca6ff6987eac66eed088d71bac6da
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-06-07 10:02:40 -0400

    Update 01_📈_Show_diagrams.py

--- pages/01_📈_Show_diagrams.py
+++ pages/01_📈_Show_diagrams.py
@@ -186,8 +186,7 @@ else:
         fig8.update_layout(title_font_size=24, showlegend=False)
         fig8.update_traces(hovertemplate='%{x}<br>MAD = %{y}')
         col8.plotly_chart(fig8, use_container_width=True)
-        
-        
+           
         
         #-------------------------------------------------------------------------------------------------------------- coffee prize history (scatter + bar chart)
         st.subheader("Prize history")
@@ -220,3 +219,82 @@ else:
         fig8.update_traces(hovertemplate='%{y}: %{x}')
         fig8.update_xaxes(showticklabels=False)
         col2.plotly_chart(fig8, use_container_width=True)
+
+        
+    #-------------------------------------------------------------------------------------------------------------- weekly coffees and breaks (line chart)
+    if c_b_weekly:
+        st.subheader("Weekly breaks and coffees")
+        columns=['Breaks','Coffees']
+        weekly_data = get_weekly_coffees_breaks(names)
+
+        weeks=[]
+        weekly_br_c=[]
+    
+        for i in range(len(weekly_data)):
+            temp=[]
+            weeks.append(weekly_data[i][0])
+            temp.append(weekly_data[i][1])
+            temp.append(weekly_data[i][2])
+            weekly_br_c.append(temp)
+
+        df = pd.DataFrame(weekly_br_c, columns=columns, index=weeks)              #weekly coffees/breaks
+
+        fig3 = px.line(df, title="Weekly coffee breaks and coffees", labels={"variable":"", "index":"", "value":""})
+        fig3.update_layout(title_font_size=24, hovermode="x unified", legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="center", x=0.5))
+        fig3.update_traces(hovertemplate='%{y}')
+        st.plotly_chart(fig3, use_container_width=True)
+
+
+    #-------------------------------------------------------------------------------------------------------------- absolute and relative correlations (bubble charts)
+    if correlation:
+        st.subheader("Correlation diagrams")
+        col3, col4 = st.columns([1,1])                        #setting up two columns for narrower charts        
+        corr_tot=get_correlation(names)
+        corr_abs_raw=corr_tot[0]
+        corr_rel_raw=corr_tot[1]
+        print(corr_tot)
+        temp1=[]
+        temp2_abs=[]
+        temp2_rel=[]
+        tickval_num=[]
+        names_inv=[]
+        for i in range(len(names)):
+            tickval_num.append(i+1)
+            names_inv.append(names[len(names)-i-1])
+            for j in range(len(names)):
+               temp_abs=[]
+               temp_rel=[]
+               temp_abs.append(i+1)
+               temp_rel.append(i+1)
+               temp_abs.append(j+1)
+               temp_rel.append(j+1)
+               #temp_abs.append(corr_abs_raw[len(names)-j-1][i])      #calculates absolute correlation
+               #temp_rel.append(corr_rel_raw[len(names)-j-1][i])      #calculates relative correlation
+               temp_abs.append(corr_abs_raw[i][len(names)-j-1])      #calculates absolute correlation
+               temp_rel.append(corr_rel_raw[i][len(names)-j-1])      #calculates relative correlation
+               temp2_abs.append(temp_abs)
+               temp2_rel.append(temp_rel)
+        columns_corr_abs=['x-values','y-values','Coffees']
+        columns_corr_rel=['x-values','y-values','Percent']
+        
+        df = pd.DataFrame(temp2_abs, columns=columns_corr_abs)
+        fig5 = px.scatter(df, x='x-values', y='y-values', size='Coffees', custom_data=['Coffees'], labels={"x-values":"", "y-values":""}, title="Absolute correlation", color='Coffees')
+        fig5.update_layout(title_font_size=24, showlegend=False, xaxis=dict(tickmode = 'array', tickvals = tickval_num, ticktext = names), yaxis=dict(tickmode = 'array', tickvals = tickval_num, ticktext = names_inv))
+        #fig5.update_traces(hovertemplate="%{y} with %{x}:<br>%{customdata[0]} coffees")
+        fig5.update_traces(hovertemplate="%{y} drank %{customdata[0]} coffees with %{x}")
+        fig5.update_xaxes(side="top")
+        col3.plotly_chart(fig5, use_container_width=True)#              absolute correlation
+        #                                                  --------------------------------------------------
+        df = pd.DataFrame(temp2_rel, columns=columns_corr_rel)
+        fig6 = px.scatter(df, x='x-values', y='y-values', size='Percent', custom_data=['Percent'], labels={"x-values":"", "y-values":""}, title="Relative correlation", color='Percent')#, text='size')
+        fig6.update_layout(title_font_size=24, showlegend=False, xaxis=dict(tickmode = 'array', tickvals = tickval_num, ticktext = names), yaxis=dict(tickmode = 'array', tickvals = tickval_num, ticktext = names_inv))
+        #fig6.update_traces(hovertemplate="%{x} with %{y}:<br>%{customdata[0]} %")
+        fig6.update_traces(hovertemplate="%{y} drank %{customdata[0]} % of<br>their coffees with %{x}")
+        fig6.update_xaxes(side="top")
+        col4.plotly_chart(fig6, use_container_width=True)
+        
+        
+        
+        
+        
+        
