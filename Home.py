8eeba8d801a4a83de48cd1015fb73c93ff27800a
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-06-07 10:22:54 -0400

    Update Home.py

--- Home.py
+++ Home.py
@@ -125,6 +125,33 @@ else:
   
     
 with st.sidebar:
-    imprint = '<p style="font-family:sans-serif; color:Grey; font-size: 12px;">Made by P. C. Brehm and T. Kalisch. Web design by T. Kalisch</p>'
-    st.footer(imprint, unsafe_allow_html=True)
+    #imprint = '<p style="font-family:sans-serif; color:Grey; font-size: 12px;">Made by P. C. Brehm and T. Kalisch. Web design by T. Kalisch</p>'
+    #st.markdown(imprint, unsafe_allow_html=True)
+    footer="""<style>
+    a:link , a:visited{
+    color: blue;
+    background-color: transparent;
+    text-decoration: underline;
+    }
 
+    a:hover,  a:active {
+    color: red;
+    background-color: transparent;
+    text-decoration: underline;
+    }
+
+    .footer {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    background-color: white;
+    color: black;
+    text-align: center;
+    }
+    </style>
+    <div class="footer">
+    <p>Developed with ❤ by <a style='display: block; text-align: center;' href="https://www.heflin.dev/" target="_blank">Heflin Stephen Raj S</a></p>
+    </div>
+    """
+    st.markdown(footer,unsafe_allow_html=True)
