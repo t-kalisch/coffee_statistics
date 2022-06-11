49effa32e72d5a9f815c72b4c622664e2ff1a839
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-06-11 15:29:55 -0400

    Update 05_📆_Enter_holidays.py

--- pages/05_📆_Enter_holidays.py
+++ pages/05_📆_Enter_holidays.py
@@ -79,62 +79,62 @@ if 'logged_in' not in st.session_state or 'user_name' not in st.session_state or
     st.warning("Warning! Your session is no longer active. Please return to home to restart it and regain access to all features.")
 else:
 
-	if st.session_state.admin == "1":
-	    col1, col2, col3, col4 = st.columns([0.5,1,1,1])
-	    month = col1.text_input("Month", placeholder=datetime.date.today().month)
-	    year = col2.text_input("Year", placeholder=datetime.date.today().year)
-	    person_hol = col3.text_input("Person", placeholder = "User")
-	    holidays = col4.text_input("Number of holidays", placeholder=0)
-	    if person_hol == "":
-		sub_hol = st.button("Submit holidays", help="Submit holidays for yourself", on_click=submit_holidays, args=(st.session_state.user_name, month, year, holidays))
-		#if sub_hol:
-		#    submit_holidays(st.session_state.user_name, month, year, holidays)
-	    else:
-		sub_hol = st.button("Submit holidays", help="Submit holidays for "+person_hol, on_click=submit_holidays, args=(person_hol,month,year,holidays))
-		#if sub_hol:
-		#    submit_holidays(person_hol, month, year, holidays)
+    if st.session_state.admin == "1":
+        col1, col2, col3, col4 = st.columns([0.5,1,1,1])
+        month = col1.text_input("Month", placeholder=datetime.date.today().month)
+        year = col2.text_input("Year", placeholder=datetime.date.today().year)
+        person_hol = col3.text_input("Person", placeholder = "User")
+        holidays = col4.text_input("Number of holidays", placeholder=0)
+        if person_hol == "":
+        sub_hol = st.button("Submit holidays", help="Submit holidays for yourself", on_click=submit_holidays, args=(st.session_state.user_name, month, year, holidays))
+        #if sub_hol:
+        #    submit_holidays(st.session_state.user_name, month, year, holidays)
+        else:
+        sub_hol = st.button("Submit holidays", help="Submit holidays for "+person_hol, on_click=submit_holidays, args=(person_hol,month,year,holidays))
+        #if sub_hol:
+        #    submit_holidays(person_hol, month, year, holidays)
 
-	    st.write("-" * 34)   
-	    st.subheader("All holidays")
-	    all_holidays = get_all_holidays(datetime.datetime.now())
-	    #print(all_holidays)
-	    names=get_members()
-	    columns=["ID","Tot. work days"]
-	    for i in range(len(names)):
-		columns.append(names[i])
-	    #st.write(all_holidays)
-	    df=pd.DataFrame(all_holidays,columns=columns)
-	    st.dataframe(df, width=1000, height=1000)
+        st.write("-" * 34)   
+        st.subheader("All holidays")
+        all_holidays = get_all_holidays(datetime.datetime.now())
+        #print(all_holidays)
+        names=get_members()
+        columns=["ID","Tot. work days"]
+        for i in range(len(names)):
+        columns.append(names[i])
+        #st.write(all_holidays)
+        df=pd.DataFrame(all_holidays,columns=columns)
+        st.dataframe(df, width=1000, height=1000)
 
-	elif st.session_state.admin == "0":
-	    col1, col2, col3 = st.columns([0.5,1,2])
-	    month = col1.text_input("Month", placeholder=datetime.date.today().month)
-	    year = col2.text_input("Year", placeholder=datetime.date.today().year)
-	    holidays = col3.text_input("Number of holidays", placeholder=0)
-	    sub_hol = col1.button("Submit", help="Submit holidays")
-	    if sub_hol:
-		submit_holidays(st.session_state.user_name, month, year, holidays)
-	    st.write("-" * 34)   
-	    st.subheader("All holidays")
-	    all_holidays = get_all_holidays(datetime.datetime.now())
-	    #print(all_holidays)
-	    holidays_person=[]
-	    names=get_members()
-	    columns=["Month","Total work days"]
-	    for i in range(len(names)):
-		if names[i] == st.session_state.user_name:
-		    columns.append(names[i])
-		    for j in range(len(all_holidays)):
-			temp=[]
-			temp.append(all_holidays[j][0])
-			temp.append(all_holidays[j][i+2])
-			temp.append(all_holidays[j][1])
-			holidays_person.append(temp)
-	    df=pd.DataFrame(holidays_person,columns=columns)
-	    st.dataframe(df, width=1000, height=1000)
+    elif st.session_state.admin == "0":
+        col1, col2, col3 = st.columns([0.5,1,2])
+        month = col1.text_input("Month", placeholder=datetime.date.today().month)
+        year = col2.text_input("Year", placeholder=datetime.date.today().year)
+        holidays = col3.text_input("Number of holidays", placeholder=0)
+        sub_hol = col1.button("Submit", help="Submit holidays")
+        if sub_hol:
+        submit_holidays(st.session_state.user_name, month, year, holidays)
+        st.write("-" * 34)   
+        st.subheader("All holidays")
+        all_holidays = get_all_holidays(datetime.datetime.now())
+        #print(all_holidays)
+        holidays_person=[]
+        names=get_members()
+        columns=["Month","Total work days"]
+        for i in range(len(names)):
+        if names[i] == st.session_state.user_name:
+            columns.append(names[i])
+            for j in range(len(all_holidays)):
+            temp=[]
+            temp.append(all_holidays[j][0])
+            temp.append(all_holidays[j][i+2])
+            temp.append(all_holidays[j][1])
+            holidays_person.append(temp)
+        df=pd.DataFrame(holidays_person,columns=columns)
+        st.dataframe(df, width=1000, height=1000)
 
-	else:
-	    st.warning("You do not have the permissions to submit holidays. Please contact a system administrator for further information.")
+    else:
+        st.warning("You do not have the permissions to submit holidays. Please contact a system administrator for further information.")
     
 
 	#------- footer ----------------
