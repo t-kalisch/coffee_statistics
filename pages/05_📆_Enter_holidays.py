2ca2b834d96c5a6fb6a45f2a1af88556e40a8bbb
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-06-11 15:31:56 -0400

    Update 05_📆_Enter_holidays.py

--- pages/05_📆_Enter_holidays.py
+++ pages/05_📆_Enter_holidays.py
@@ -86,13 +86,9 @@ else:
         person_hol = col3.text_input("Person", placeholder = "User")
         holidays = col4.text_input("Number of holidays", placeholder=0)
         if person_hol == "":
-        sub_hol = st.button("Submit holidays", help="Submit holidays for yourself", on_click=submit_holidays, args=(st.session_state.user_name, month, year, holidays))
-        #if sub_hol:
-        #    submit_holidays(st.session_state.user_name, month, year, holidays)
+            sub_hol = st.button("Submit holidays", help="Submit holidays for yourself", on_click=submit_holidays, args=(st.session_state.user_name, month, year, holidays))
         else:
-        sub_hol = st.button("Submit holidays", help="Submit holidays for "+person_hol, on_click=submit_holidays, args=(person_hol,month,year,holidays))
-        #if sub_hol:
-        #    submit_holidays(person_hol, month, year, holidays)
+            sub_hol = st.button("Submit holidays", help="Submit holidays for "+person_hol, on_click=submit_holidays, args=(person_hol,month,year,holidays))
 
         st.write("-" * 34)   
         st.subheader("All holidays")
@@ -101,7 +97,7 @@ else:
         names=get_members()
         columns=["ID","Tot. work days"]
         for i in range(len(names)):
-        columns.append(names[i])
+            columns.append(names[i])
         #st.write(all_holidays)
         df=pd.DataFrame(all_holidays,columns=columns)
         st.dataframe(df, width=1000, height=1000)
@@ -113,7 +109,7 @@ else:
         holidays = col3.text_input("Number of holidays", placeholder=0)
         sub_hol = col1.button("Submit", help="Submit holidays")
         if sub_hol:
-        submit_holidays(st.session_state.user_name, month, year, holidays)
+            submit_holidays(st.session_state.user_name, month, year, holidays)
         st.write("-" * 34)   
         st.subheader("All holidays")
         all_holidays = get_all_holidays(datetime.datetime.now())
@@ -122,14 +118,14 @@ else:
         names=get_members()
         columns=["Month","Total work days"]
         for i in range(len(names)):
-        if names[i] == st.session_state.user_name:
-            columns.append(names[i])
-            for j in range(len(all_holidays)):
-            temp=[]
-            temp.append(all_holidays[j][0])
-            temp.append(all_holidays[j][i+2])
-            temp.append(all_holidays[j][1])
-            holidays_person.append(temp)
+            if names[i] == st.session_state.user_name:
+                columns.append(names[i])
+                for j in range(len(all_holidays)):
+                    temp=[]
+                    temp.append(all_holidays[j][0])
+                    temp.append(all_holidays[j][i+2])
+                    temp.append(all_holidays[j][1])
+                holidays_person.append(temp)
         df=pd.DataFrame(holidays_person,columns=columns)
         st.dataframe(df, width=1000, height=1000)
 
