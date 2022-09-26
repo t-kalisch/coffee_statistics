323625430b3adea3917f8f7ae96a862169abe9c1
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-09-26 15:55:36 +0200

    Update calculations.py

--- calculations.py
+++ calculations.py
@@ -230,7 +230,7 @@ def get_corr_time(names, month_id_start, month_id_end):
             n_corr_rel = 0
             n_tot_coffees = 0
             for k in range(len(tmp)):
-                n_corr += tmp[i][k][j+2]
+                n_corr += int(tmp[i][k][j+2])
             all_corr_data_person.append(n_corr)
         all_corr_data.append(all_corr_data_person)
 		
