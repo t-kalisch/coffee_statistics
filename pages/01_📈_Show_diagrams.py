ebc1329cd47d286759763ec4e1e33562484c8c2a
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-09-26 16:34:19 +0200

    Update 01_📈_Show_diagrams.py

--- pages/01_📈_Show_diagrams.py
+++ pages/01_📈_Show_diagrams.py
@@ -335,11 +335,8 @@ else:
               month_end += str(timespan[1].month)
           
           corr_tot_time = get_corr_time(names, month_start, month_end)
-          st.write(corr_tot_time)
-          
-          corr_tot=get_correlation(names)
-          corr_abs_raw=corr_tot[0]
-          corr_rel_raw=corr_tot[1]
+          corr_abs_raw=corr_tot_time[0]
+          corr_rel_raw=corr_tot_time[1]
           
           temp1=[]
           temp2_abs=[]
