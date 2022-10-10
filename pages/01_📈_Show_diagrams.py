21f9ba4bb10ee1d490519df2b3eab0c7e843794f
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-10-10 18:56:34 +0200

    Update 01_📈_Show_diagrams.py

--- pages/01_📈_Show_diagrams.py
+++ pages/01_📈_Show_diagrams.py
@@ -112,12 +112,12 @@ else:
         monthly_coffees_all = [ x.strip() for x in all_data[0].strip(';').split(';') ]
         
         for i in range(len(monthly_coffees_all)):
-          if monthly_coffees_all[i].indexOf('_')
-          monthly_coffees_all[i] = [ x.strip() for x in monthly_coffees_all[i].strip('_').split('_') ]
+          if monthly_coffees_all[i].indexOf('_'):
+            monthly_coffees_all[i] = [ x.strip() for x in monthly_coffees_all[i].strip('_').split('_') ]
           st.write(monthly_coffees_all)
           for j in range(len(monthly_coffees_all[i])):
-            if monthly_coffees_all[i][j].indexOf(',')
-            monthly_coffees_all[i][j] = [ x.strip() for x in monthly_coffees_all[i][j].strip(',').split(',') ]
+             if monthly_coffees_all[i][j].indexOf(',')
+               monthly_coffees_all[i][j] = [ x.strip() for x in monthly_coffees_all[i][j].strip(',').split(',') ]
 
 
         st.write(monthly_coffees_all)
