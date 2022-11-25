27105fd3c28f17f7c80ad0f07bbd29688a16f064
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-11-25 09:43:56 +0100

    Update 02_☕_Submit_coffee_or_break.py

--- pages/02_☕_Submit_coffee_or_break.py
+++ pages/02_☕_Submit_coffee_or_break.py
@@ -99,8 +99,9 @@ def add_coffee_to_break(id_ext, name, user):
 	if name == "":
 		name = user
 	cursor.execute("select persons, coffees from drinkers where id_ext = '"+id_ext+"'")
-	st.write(list(cursor.fetchall()[0]))
+	#st.write(list(cursor.fetchall()[0]))
 	drinker_data=list(cursor.fetchall()[0])
+	st.write(drinker_data)
 	if drinker_data == []:
 		st.warning("Invalid extended ID")
 		return
