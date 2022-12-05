fc26799a23be95095d56f396a2d257883a6d51e5
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-12-05 09:27:29 +0100

    Update 01_📈_Show_diagrams.py

--- pages/01_📈_Show_diagrams.py
+++ pages/01_📈_Show_diagrams.py
@@ -323,6 +323,7 @@ else:
               #if weekly_data[i][3] == Null:
               #    temp.append(float(0))
               #else:
+              st.write(weekly_data)
               temp.append(float(weekly_data[i][3]))
               weekly_br_c.append(temp)
 
