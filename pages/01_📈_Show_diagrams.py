30008086562abc5e1c23b315c41624ab9cd9c1ae
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-12-23 09:51:01 +0100

    Update 01_📈_Show_diagrams.py

--- pages/01_📈_Show_diagrams.py
+++ pages/01_📈_Show_diagrams.py
@@ -254,7 +254,7 @@ else:
           st.subheader("Prize history")
           col1, col2 = st.columns([2,1])
           prizes = get_prizes(names, month_id_dly, act_func)
-
+          st.write(prizes)
           tickval_num=[]
           total_prizes=[]
 
@@ -284,7 +284,7 @@ else:
                       else:
                           temp.append(sum)
                   total_prizes.append(temp)
-          st.write(total_prizes)
+          
 
           columns=['Month','Persons','Coffee prizes','sizes']
           df = pd.DataFrame(prizes, columns=columns)
