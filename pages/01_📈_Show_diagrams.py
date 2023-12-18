b467cb9decf2f002d1b264884139f32c85f14b53
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2023-12-18 12:26:48 +0100

    Update 01_📈_Show_diagrams.py

--- pages/01_📈_Show_diagrams.py
+++ pages/01_📈_Show_diagrams.py
@@ -6,6 +6,24 @@ from datetime import date
 from calculations import *
 import plotly.graph_objects as go
 
+from contextlib import contextmanager, redirect_stdout
+from io import StringIO
+def st_capture(output_func):
+    with StringIO() as stdout, redirect_stdout(stdout):
+        old_write = stdout.write
+
+        def new_write(string):
+            ret = old_write(string)
+            output_func(stdout.getvalue())
+            return ret
+        
+        stdout.write = new_write
+        yield
+
+
+output = st.empty()
+
+
   #------------------------ getting functionals from database ------------------
 def get_functionals():
     db = init_connection()
